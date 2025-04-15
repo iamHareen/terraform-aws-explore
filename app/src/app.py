5c0841153a9f72045d165f586dc5bdf363e570a1
iamHareen <helanm06@gmail.com>
2025-04-16 02:19:01 +0530

    remove unwanted comments

--- app/src/app.py
+++ app/src/app.py
@@ -9,7 +9,5 @@ def hello():
     return "Hello, I'm Hareen Helanjith"
 
 if __name__ == '__main__':
-    # Get port from environment variable or default to 5000
     port = int(os.environ.get('PORT', 5001))
-    # Run the application on 0.0.0.0 to be accessible outside the container
     app.run(host='0.0.0.0', port=port)
